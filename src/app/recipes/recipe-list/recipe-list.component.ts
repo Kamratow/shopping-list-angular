3679a9a1da3918ae6f92bcf520953ad359bcc54c
Kamratow <kamilratowski90@gmail.com>
2019-02-25 14:54:15 +0100

    Added basic template for the recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html',
 	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes = [];
+	recipes: Recipe[] = [
+		new Recipe('A test Recipe', 'This is a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+	];
 
 	constructor() { }
 
