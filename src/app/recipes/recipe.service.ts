95883b43312881000b051a4ba7498f4c321543c2
Kamratow <kamilratowski90@gmail.com>
2019-02-27 13:25:44 +0100

    Added service files. Implemented fetching recipes from RecipeService

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,13 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+		new Recipe('A test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+		new Recipe('Another test Recipe', 'This is another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+	];
+
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
