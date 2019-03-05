6c574459b5d0734d2e70614dea50ecba4af445e4
Kamratow <kamilratowski90@gmail.com>
2019-03-05 19:02:00 +0100

    Implemented saving recipes in the firebase database

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataStorageService {
+    constructor(private http: Http,
+                private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        return this.http.put('https://my-ng-recipe-book-b0167.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    }
+}
