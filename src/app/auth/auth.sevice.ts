0f04d54a219ffb00e713c2df9502fb167185a468
Kamratow <kamilratowski90@gmail.com>
2019-03-06 11:26:17 +0100

    Implemented using toke to authenticate database requests

--- src/app/auth/auth.sevice.ts
+++ src/app/auth/auth.sevice.ts
@@ -5,6 +5,8 @@ import * as firebase from 'firebase';
     providedIn: 'root'
 })
 export class AuthService {
+    token: string;
+
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .catch(
@@ -15,10 +17,23 @@ export class AuthService {
     signInUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
-                response => console.log(response)
+                response => {
+                    firebase.auth().currentUser.getIdToken()
+                        .then(
+                            (token: string) => this.token = token
+                        )
+                }
             )
             .catch(
                 error => console.log(error)
             );
+    }
+
+    getToken() {
+        firebase.auth().currentUser.getIdToken()
+            .then(
+                (token: string) => this.token = token
+            );
+        return this.token;
     }
 }
