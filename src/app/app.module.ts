05d631bdf5de6a129ba96abfa90b48e42ae3d406
Kamratow <kamilratowski90@gmail.com>
2019-03-06 15:41:07 +0100

    Added a lazy loading for the Recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -20,7 +19,6 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     HttpModule,
-    RecipesModule,
     SharedModule,
     ShoppingListModule,
     AuthModule,
