703b3c51ea5abd54d353174fd878249ec12dac40
Kamratow <kamilratowski90@gmail.com>
2019-03-06 10:46:12 +0100

    Implemented login mechanism

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../auth.sevice';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+	selector: 'app-signin',
+	templateUrl: './signin.component.html',
+	styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+	constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	onSignin(form: NgForm) {
+		const email = form.value.email;
+		const password = form.value.password;
+		this.authService.signInUser(email, password);
+	}
 
 }
