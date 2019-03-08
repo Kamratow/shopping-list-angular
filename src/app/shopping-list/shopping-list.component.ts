c08e41e63a0369daf38dd203deb78ea69d8c6fcb
Kamratow <kamilratowski90@gmail.com>
2019-03-08 12:26:00 +0100

    Expanded app state to monitor edited item

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Observable } from 'rxjs';
+import * as fromShoppingList from './store/shopping-list.reducers';
 
 @Component({
 	selector: 'app-shopping-list',
@@ -13,7 +14,7 @@ import { Observable } from 'rxjs';
 export class ShoppingListComponent implements OnInit {
 	shoppingListState: Observable<{ ingredients: Ingredient[] }>;
 
-	constructor(private slService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
+	constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
 
 	ngOnInit() {
 		this.shoppingListState = this.store.select('shoppingList');
