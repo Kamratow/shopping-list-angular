8be6405011e57068dbfeb04e4e4d851ca0fc42d7
Kamratow <kamilratowski90@gmail.com>
2019-03-08 14:59:38 +0100

    Added a basic file setup needed to introduce auth reducer

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -2,14 +2,10 @@ import * as ShoppingListActions from './shopping-list.actions';
 
 import { Ingredient } from '../../shared/ingredient.model';
 
-export interface AppState {
-    shoppingList: State
-}
-
 export interface State {
     ingredients: Ingredient[],
     editedIngredient: Ingredient,
-    editedIngredientIndex: number;
+    editedIngredientIndex: number
 }
 
 const initialState: State = {
