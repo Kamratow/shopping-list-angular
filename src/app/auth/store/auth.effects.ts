d985448f50ead4fab05288f4dd4d44bbb8062617
Kamratow <kamilratowski90@gmail.com>
2019-03-11 09:11:06 +0100

    Added a singup auth effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions, ofType } from '@ngrx/effects';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+import { from } from 'rxjs';
+import * as firebase from 'firebase';
 
 import * as AuthActions from './auth.actions';
 
@@ -8,8 +11,28 @@ export class AuthEffects {
     @Effect()
     authSignup = this.actions$
         .pipe(
-            ofType(AuthActions.TRY_SIGNUP)
+            ofType(AuthActions.TRY_SIGNUP),
+            map((action: AuthActions.TrySignup) => {
+                return action.payload;
+            }),
+            switchMap((authData: { username: string, password: string }) => {
+                return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+            }),
+            switchMap(() => {
+                return from(firebase.auth().currentUser.getIdToken());
+            }),
+            mergeMap((token: string) => {
+                return [
+                    {
+                        type: AuthActions.SIGNUP
+                    },
+                    {
+                        type: AuthActions.SET_TOKEN,
+                        payload: token
+                    }
+                ];
+            })
         );
 
-    constructor(private actions$: Actions) {}
+    constructor(private actions$: Actions) { }
 }
