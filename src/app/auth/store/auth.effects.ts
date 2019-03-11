0d84f891e5f01cf5c350dcd4479279a863ce2cc7
Kamratow <kamilratowski90@gmail.com>
2019-03-11 09:52:26 +0100

    Added a signin effect with redirect to main page

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import { map, switchMap, mergeMap } from 'rxjs/operators';
 import { from } from 'rxjs';
@@ -34,5 +35,32 @@ export class AuthEffects {
             })
         );
 
-    constructor(private actions$: Actions) { }
+    @Effect()
+    authSignin = this.actions$
+        .pipe(
+            ofType(AuthActions.TRY_SIGNIN),
+            map((action: AuthActions.TrySignup) => {
+                return action.payload;
+            }),
+            switchMap((authData: { username: string, password: string }) => {
+                return from(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+            }),
+            switchMap(() => {
+                return from(firebase.auth().currentUser.getIdToken());
+            }),
+            mergeMap((token: string) => {
+                this.router.navigate(['/']);
+                return [
+                    {
+                        type: AuthActions.SIGNIN
+                    },
+                    {
+                        type: AuthActions.SET_TOKEN,
+                        payload: token
+                    }
+                ];
+            })
+        );
+
+    constructor(private actions$: Actions, private router: Router) { }
 }
